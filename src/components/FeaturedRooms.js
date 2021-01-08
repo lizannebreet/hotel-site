ac3bc132da89a4d251153513f27ccc2cad66f53f
Lizanne Breet <lizannebreet@gmail.com>
2021-01-08 14:23:49 +0200

    Context(feat): add context

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { RoomContext } from '../Context';
+
+export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
+  render() {
+    const {name, greeting} = this.context;
+    return <div> {greeting} {name} from Featured rooms</div>
+  }
+}
