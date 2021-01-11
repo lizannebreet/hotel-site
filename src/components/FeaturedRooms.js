6f08f38c824879a890186d3ebdf1c85ddf9445d4
Lizanne Breet <lizannebreet@gmail.com>
2021-01-11 15:35:45 +0200

    FeaturedRooms(feat): add rooms and title

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -7,7 +7,7 @@ import Title from './Title';
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { loading, featuredRooms: rooms } = this.context;
+    let { loading, featuredRooms: rooms } = this.context;
     rooms = rooms.map(room => {
       return <Room key={room.id} room={room} />
     })
@@ -15,9 +15,8 @@ export default class FeaturedRooms extends Component {
       <section className='featured-rooms'>
         <Title title='featured rooms' />
         <div className='featured-rooms-center'>
-          { }
+          {loading ? <Loading /> : rooms}
         </div>
-        <Loading />
       </section>
     );
   }
--- src/components/Room.js
+++ src/components/Room.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import defaultImg from "../images/room-1.jpeg";
+import PropTypes from "prop-types";
 
-function Room() {
-  return <div>hello from room</div>
+function Room({ room }) {
+  const { name, slug, images, price } = room;
+  return (
+    <article className='room'>
+      <div className="img-container">
+        <img src={images[0] || defaultImg} alt="single room" />
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+      </Link>
+        <p className="room-info">{name}</p>
+      </div>
+    </article>
+  );
 }
+
+//check properties to see if they are there and if not show warning
+Room.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    price: PropTypes.number.isRequired
+  })
+};
 
 export default Room;
