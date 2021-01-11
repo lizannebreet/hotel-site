ff86bc7231d8ddc697d878cd171d75544c5e79a6
Lizanne Breet <lizannebreet@gmail.com>
2021-01-11 13:32:04 +0200

    FeaturedRooms(feat): add rooms and title

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../Context';
+import Loading from './Loading';
+import Room from './Room';
+import Title from './Title';
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const {name, greeting} = this.context;
-    return <div> {greeting} {name} from Featured rooms</div>
+    const { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />
+    })
+    return (
+      <section className='featured-rooms'>
+        <Title title='featured rooms' />
+        <div className='featured-rooms-center'>
+          { }
+        </div>
+        <Loading />
+      </section>
+    );
   }
 }
