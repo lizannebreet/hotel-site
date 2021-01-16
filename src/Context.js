104c74ced35f7c85b88e4b00e57fb5e9eb67da9a
Lizanne Breet <lizannebreet@gmail.com>
2021-01-16 16:15:12 +0200

    RoomFilter(feat): finished the rooms filters

--- src/Context.js
+++ src/Context.js
@@ -62,9 +62,56 @@ class RoomProvider extends Component {
     const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
     console.log(name, value);
+
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
   };
   filterRooms = () => {
-    console.log('hello');
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+
+    let tempRooms = [...rooms];
+    // transform values
+    // get capacity
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+    // filter by type
+    if (type !== "all") {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+    }
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+    //filter by size
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
+    this.setState({
+      sortedRooms: tempRooms
+    });
   };
 
   render() {
@@ -84,6 +131,8 @@ class RoomProvider extends Component {
 
 const RoomConsumer = RoomContext.Consumer;
 
+export { RoomProvider, RoomConsumer, RoomContext };
+
 export function withRoomConsumer(Component) {
   return function ConsumerWrapper(props) {
     return (
@@ -93,5 +142,3 @@ export function withRoomConsumer(Component) {
     );
   };
 }
-
-export { RoomProvider, RoomConsumer, RoomContext };
